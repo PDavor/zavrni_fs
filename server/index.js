d81aa074dcea774ca562a4200664d98edb4bc565
Davor Pernjek <pernjek.davor12@gmail.com>
2021-06-08 11:35:23 +0200

    i

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import rjesavaRoutes from "./routes/rjesavaRoute.js";
 const app = express(); //app, pokretanje express-a
 app.use(cors());
 app.use(express.json());
+app.get('/', (req, res) => { res.send('Hello from Express!')
 app.use("/kategorije", kategorijaRoutes); //svaki route iz kategorija.js pocinje s /kategorija
 app.use("/auth", authRoutes);
 app.use("/rjesava", rjesavaRoutes);
@@ -21,7 +22,7 @@ const CONNECTION_URL =
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.MONGODB_URI || CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
